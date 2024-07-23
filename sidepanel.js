24c13d1e89c8d2353b52c0e4e124f754d540d7c0
vanad1um4 <Vanad1um3@gmail.com>
2024-07-24 00:11:01 +0400

    🏗  major improvements

--- sidepanel.js
+++ sidepanel.js
@@ -1,83 +1,158 @@
-const priceFractionDiv = document.getElementById('priceFraction');
+// let isScriptRunning = false;
+const boostFractionDiv = document.getElementById('boostFraction');
+const boostScrapedDiv = document.getElementById('boostScraped');
+const boostParsedDiv = document.getElementById('boostParsed');
+const priceScrapedDiv = document.getElementById('priceScraped');
+const priceParsedDiv = document.getElementById('priceParsed');
 
-document.getElementById('startButton').addEventListener('click', () => {
+document.getElementById('injectButton').addEventListener('click', () => {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     chrome.scripting.executeScript({
       target: { tabId: tabs[0].id, allFrames: true },
-      func: init,
+      func: mainInjectable,
     });
   });
 });
 
-// document.getElementById('stopButton').addEventListener('click', () => {
-//   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-//     chrome.tabs.sendMessage(tabs[0].id, { action: 'stopClicking' });
-//   });
-// });
-
-document.getElementById('tapButton').addEventListener('click', () => {
+document.getElementById('startButton').addEventListener('click', () => {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    chrome.scripting.executeScript({
-      target: { tabId: tabs[0].id, allFrames: true },
-      func: tapButton,
-    });
+    chrome.tabs.sendMessage(tabs[0].id, { action: 'startScript' });
+  });
+});
+
+document.getElementById('tapNowButton').addEventListener('click', () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id, { action: 'tapNow' });
   });
 });
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === 'buttonClicked') {
-  } else if (message.action === 'tick') {
-    if (message.message.priceFraction) {
-      priceFractionDiv.textContent = message.message.priceFraction;
-    } else {
-      priceFractionDiv.textContent = '-';
-    }
+  switch (message.action) {
+    case 'upgradeDigits':
+      applyDigits(message.message?.upgradeDigits);
+      break;
+
+    case 'energy':
+      console.log(message.message);
+      break;
+
+    default:
+      break;
   }
 });
 
-function tapButton() {
-  if (window.location.href.includes('hamsterkombatgame.io')) {
-    const button = document.querySelector('.user-tap-button');
-    if (button) {
-      button.dispatchEvent(new PointerEvent('pointerup'));
-      // console.log('Button clicked');
-      // chrome.runtime.sendMessage({
-      //   action: 'buttonClicked',
-      //   message: 'Button was successfully clicked!',
-      // });
-    } else {
-      // console.log('Button not found');
-      // chrome.runtime.sendMessage({
-      //   action: 'buttonClicked',
-      //   message: 'Button was not found.',
-      // });
-    }
+function applyDigits(upgradeDigits) {
+  const parsedBoost = parseDigits(upgradeDigits?.scrapedBoost);
+  const scrapedPrice = parseDigits(upgradeDigits?.scrapedPrice);
+  if (parsedBoost && scrapedPrice) {
+    const boostFraction = String(Math.round((parsedBoost / scrapedPrice) * 10000) / 100) + '%';
+
+    boostFractionDiv.textContent = boostFraction;
+    boostScrapedDiv.textContent = upgradeDigits.scrapedBoost;
+    boostParsedDiv.textContent = parsedBoost;
+    priceScrapedDiv.textContent = upgradeDigits.scrapedPrice;
+    priceParsedDiv.textContent = scrapedPrice;
   } else {
-    // console.log('Not in the correct iframe');
+    boostFractionDiv.textContent = 0;
+    boostScrapedDiv.textContent = 0;
+    boostParsedDiv.textContent = 0;
+    priceScrapedDiv.textContent = 0;
+    priceParsedDiv.textContent = 0;
   }
 }
 
-function init() {
+function parseDigits(str) {
+  if (str) {
+    const parsedStr = str.replace(/[, +]/g, '');
+    return parsedStr.endsWith('K') ? parseFloat(parsedStr) * 1000 : parseFloat(parsedStr);
+  }
+  return null;
+}
+
+/// INJECTABLE /////////////////////////////////////////////////////////////////
+
+function mainInjectable() {
+  let timeoutId;
+  let isTickRunning = false;
+  let startTapping = false;
+  const button = document.querySelector('.user-tap-button');
+  const energyElem = document.querySelector('.user-tap-energy > p');
+
+  if (energyElem) {
+    sendEnergyReadings();
+  }
+
+  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.action === 'startScript' && !isTickRunning) {
+      timeoutId = setTimeout(tick, 1000);
+      isTickRunning = true;
+    }
+
+    if (message.action === 'tapNow') {
+      startTapping = true;
+    }
+  });
+
   function tick() {
     if (window.location.href.includes('hamsterkombatgame.io')) {
-      const price = document.querySelector('.bottom-sheet-scroll .upgrade-buy-info .price-value');
-      const increment = document.querySelector('.bottom-sheet-scroll .upgrade-buy-stats .price-value');
-      if (price && increment) {
-        const priceNum = parseInt(price.textContent.replace(/[.,+]/g, ''));
-        const incrementNum = parseInt(increment.textContent.replace(/[.,+]/g, ''));
-        const priceFraction = String(Math.round((incrementNum / priceNum) * 10000) / 100) + '%';
-        chrome.runtime.sendMessage({
-          action: 'tick',
-          message: { priceFraction: priceFraction },
-        });
-      } else {
-        chrome.runtime.sendMessage({
-          action: 'tick',
-          message: { priceFraction: null },
-        });
+      const tappingSectionClasses = document.querySelector('nav.app-bar-nav').children[0].classList;
+      if (tappingSectionClasses.contains('router-link-active') && startTapping) {
+        startTapping = false;
+        tap4times();
+      }
+
+      const upgradesSectionClasses = document.querySelector('nav.app-bar-nav').children[1].classList;
+      if (upgradesSectionClasses.contains('router-link-active')) {
+        for (let i = 0; i < 10; i++) {
+          setTimeout(() => {
+            tryReadUpgradesNumbers();
+          }, i * 100);
+        }
       }
     }
-    setTimeout(tick, 100);
+
+    timeoutId = setTimeout(tick, 1000);
   }
-  tick();
+
+  function tryReadUpgradesNumbers() {
+    const priceElem = document.querySelector('.bottom-sheet-scroll .upgrade-buy-info .price-value');
+    const boostElem = document.querySelector('.bottom-sheet-scroll .upgrade-buy-stats .price-value');
+    if (priceElem && boostElem) {
+      chrome.runtime.sendMessage({
+        action: 'upgradeDigits',
+        message: { upgradeDigits: { scrapedPrice: priceElem.textContent, scrapedBoost: boostElem.textContent } },
+      });
+    } else {
+      chrome.runtime.sendMessage({
+        action: 'upgradeDigits',
+        message: { upgradeDigits: null },
+      });
+    }
+  }
+
+  function tap4times() {
+    const currEnergy = Number(energyElem.textContent.split('/')[0]);
+
+    if (currEnergy > 50) {
+      button.dispatchEvent(new PointerEvent('pointerup'));
+      button.dispatchEvent(new PointerEvent('pointerup'));
+      button.dispatchEvent(new PointerEvent('pointerup'));
+      button.dispatchEvent(new PointerEvent('pointerup'));
+
+      setTimeout(tap4times, 1000 / (6 + Math.random())); // примерно 6-7 нажатий в секунду
+    } else {
+      startTapping = false;
+      sendEnergyReadings();
+    }
+  }
+
+  function sendEnergyReadings() {
+    chrome.runtime.sendMessage({
+      action: 'energy',
+      message: {
+        currEnergy: parseInt(energyElem.textContent.split('/')[0]),
+        maxEnergy: parseInt(energyElem.textContent.split('/')[1]),
+      },
+    });
+  }
 }
